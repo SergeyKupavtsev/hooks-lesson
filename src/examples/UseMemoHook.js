ec285e3572873f2dc8b589a568c48d14450ac81a
SergeyKupavtsev <kupavtsev997@gmail.com>
2023-02-02 11:57:21 +0700

    useMemo

--- src/examples/UseMemoHook.js
+++ src/examples/UseMemoHook.js
@@ -1,35 +1,43 @@
 import React, { useEffect, useMemo, useState } from 'react';
-
-
-function lazyCompute(num) {
-  console.log('lazyCompute')
+import "./styles.css"
+function lazyCompute(number) {
+  console.log('Вычисляем число')
   let i = 0
-  while (i < 99999) i++
-  return num * 2
+  while (i < 999999999) i++
+  while (i < 999999999) i++
+  while (i < 999999999) i++
+  while (i < 999999999) i++
+  return number * 3
 }
 
 const UseMemoHook = () => {
   const [number, setNumber] = useState(42)
   const [colored, setColored] = useState(false)
 
+  // const styles = {
+  //   backgroundColor: colored ? 'darkred' : '#f1f1f1'
+  // }
+
   const styles = useMemo(() => ({
-    color: colored ? 'darkred' : 'black'
+    backgroundColor: colored ? 'darkred' : '#f1f1f1'
   }), [colored])
 
+
   const computed = useMemo(() => {
-    return lazyCompute(number)
+    return lazyCompute(number);
   }, [number])
 
   useEffect(() => {
-    console.log('Styles changed')
+    console.log('useEffect! Зависимый от styles')
   }, [styles])
 
   return (
     <>
+      <h1>с useMemo</h1>
       <h1 style={styles}>Вычисляемое свойство: {computed}</h1>
-      <button className={'btn btn-success'} onClick={() => setNumber(prev => prev + 1)}>Добавить</button>
-      <button className={'btn btn-danger'} onClick={() => setNumber(prev => prev - 1)}>Убрать</button>
-      <button className={'btn btn-warning'} onClick={() => setColored(prev => !prev)}>Изменить</button>
+      <button className='button' onClick={() => setNumber(prev => prev + 1)}>Добавить</button>
+      <button className='button' onClick={() => setNumber(prev => prev - 1)}>Убрать</button>
+      <button className='button' onClick={() => setColored(prev => !prev)}>Изменить</button>
     </>
   )
 };
